e65ef3ff9dd78c23318dd4d458e4098ea2819efd
tolgabylk <tolga.beylik.tb@gmail.com>
2022-03-10 13:12:52 +0100

    adding db conf file

--- index.php
+++ index.php
@@ -3,3 +3,21 @@
 
 use Illuminate\Database\Capsule\Manager as DB;
 require_once 'vendor/autoload.php';
+
+session_start();
+
+if(!isset($_SESSION['user'])){
+    $_SESSION['user']=-1;
+}
+
+$c = [
+    'settings' => [
+        'displayErrorDetails' => true]
+];
+$cc = new \Slim\Container($c);
+$app = new \Slim\App($cc);
+$db = new DB();
+$db->addConnection(parse_ini_file('src/conf/db.conf.ini'));
+
+$db->setAsGlobal();
+$db->bootEloquent();
